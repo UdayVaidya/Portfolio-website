0c89733953b8e4245efac99da636000471bd52a0
udayvaidya13 <udayvaidya13@gmail.com>
2024-09-30 00:56:36 +0530

    intial commit

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css"
+
+function Navbar() {
+    return(
+        <header class="header">
+        <div class="logo-container">
+        </div>
+        <nav class="navbar">
+          
+        </nav>
+    </header>
+    );
+}
+
+export default Navbar;
